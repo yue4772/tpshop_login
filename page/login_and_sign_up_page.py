342934dca51fd30d7fed8400dee9a7f042b2dd69
yue4772 <mumu.991992@qq.com>
2019-03-12 22:46:40 +0800

    只输入用户名和密码的脚本，并做了随机数的参数化和截图

--- page/login_and_sign_up_page.py
+++ page/login_and_sign_up_page.py
@@ -9,6 +9,8 @@ class LoginandsignupPage(BaseAction):
     phone_edit_text = By.ID, "com.tpshop.malls:id/edit_phone_num"
     password_edit_text = By.ID, "com.tpshop.malls:id/edit_password"
     login_button = By.ID, "com.tpshop.malls:id/btn_login"
+    # 显示密码按钮
+    show_password_button = By.ID, "com.tpshop.malls:id/img_view_pwd"
 
     @allure.step(title="输入电话号码")
     def input_phone(self, text):
@@ -22,6 +24,10 @@ class LoginandsignupPage(BaseAction):
     def click_login(self):
         self.click(self.login_button)
 
+    @allure.step(title="点击显示密码")
+    def click_show_password(self):
+        self.click(self.show_password_button)
+
 
     def is_login(self, content):
         try:
@@ -30,3 +36,22 @@ class LoginandsignupPage(BaseAction):
         except Exception:
             return False
 
+    def is_login_enable(self):
+        """
+        判断 登录按钮中的enabled属性是否可用
+        :return: 是否可用
+        """
+        return self.find_element(self.login_button).get_attribute("enabled") == "true"
+
+        # # if self.find_element(self.login_button).get_attribute("enabled") =="true":
+        # #     return True
+        # # else:
+        # #     return False
+
+    def is_passwod_exist(self, password):
+        """
+        根据传入的密码，判断是否和密码框上的密码一致
+        :param password: 传入的密码
+        :return: 是否一致
+        """
+        return self.find_element(self.password_edit_text).text == password
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,8 +1,20 @@
+import random
+
+import allure
 import pytest
+import time
 
 from base.base_analyze import analyze_file
 from base.base_driver import init_driver
 from page.page import Page
+
+# 随机密码函数
+def random_password():
+    password=''
+    for i in range(8):
+        password += str(random.randint(0,9))
+    return password
+
 
 
 class TestLogin:
@@ -14,17 +26,40 @@ class TestLogin:
     def teardown(self):
         self.driver.quit()
 
-    @pytest.mark.parametrize(("args"), analyze_file("login_data", "test_login"))
-    def test_login(self,args):
+    # @pytest.mark.parametrize(("args"), analyze_file("login_data", "test_login"))
+    # def test_login(self,args):
+    #     self.page.home.click_mine()
+    #     self.page.mine.click_login_and_sign_up()
+    #     self.page.login_and_sign_up.input_phone(args["phone"])
+    #     self.page.login_and_sign_up.input_password(args["password"])
+    #     self.page.login_and_sign_up.click_login()
+    #     assert self.page.login_and_sign_up.is_login(args["expect"])
+
+    # @pytest.mark.parametrize(("args"), analyze_file("login_data", "test_login_miss_part"))
+    # def test_login_miss_part(self, args):
+    #     self.page.home.click_mine()
+    #     self.page.mine.click_login_and_sign_up()
+    #     self.page.login_and_sign_up.input_phone(args["phone"])
+    #     self.page.login_and_sign_up.input_password(args["password"])
+    #     # 定位登录按钮是否可用、
+    #     assert  not self.page.login_and_sign_up.is_login_enable()
+
+
+    @pytest.mark.parametrize("password",[random_password(), random_password()])
+    def test_show_passwod(self, password):
         self.page.home.click_mine()
         self.page.mine.click_login_and_sign_up()
-        self.page.login_and_sign_up.input_phone(args["phone"])
-        self.page.login_and_sign_up.input_password(args["password"])
-        self.page.login_and_sign_up.click_login()
-        assert self.page.login_and_sign_up.is_login(args["expect"])
+        self.page.login_and_sign_up.input_password(password)
+        # 判断密码找不到
+        assert not self.page.login_and_sign_up.is_passwod_exist(password)
+        # 点击显示密码按钮
+        self.page.login_and_sign_up.click_show_password()
+        time.sleep(1)
+        allure.attach("截图", self.driver.get_screenshot_as_png(), allure.attach_type.PNG)
+        # 判断密码找到，找到就成功
+        assert self.page.login_and_sign_up.is_passwod_exist(password)
 
 
-        # self.page.login_and_sign_up_page.input_phone(phone)
-        # self.page.login_and_sign_up_page.input_password(password)
-        # self.page.login_and_sign_up_page.click_login()
-        # assert self.page.login_and_sign_up_page.is_login(expect)
+
+
+
